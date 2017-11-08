123c66ca9f931ceeebf4672702d075e42cac579e
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-08 13:02:25 -0500

    copied it

---
+++ ShapeLab/src/Learnjava/CopyArrays.java
@@ -0,0 +1,42 @@
+package Learnjava;
+
+public class CopyArrays {
+	public static void main (String[] args)
+	{
+		int[] intArray = new int[10];
+		
+		for (int i = 0; i < intArray.length; i++)
+		{
+			intArray[i] = (int)(Math.random()*10);
+		}
+		int[] copy = copyArray(intArray);
+		
+		copy[copy.length-1] = -7;
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																	ZZ	Z																																																																																																																		Z																																																																													Z																																														
+		System.out.println("Array 1: ");
+		printArray(intArray);
+		
+		System.out.println("Copy of Array 1:");
+		printArray(copy);
+	}
+	
+	public static int[] copyArray(int[] intArray)
+	{
+		int[] copy = new int[intArray.length];
+		
+		for (int i = 0; i<intArray.length; i++)
+		{
+			copy[i] = intArray[i];
+ 		}
+		return copy;
+	}
+	
+	public static void printArray(int[] arr)
+	{
+		for (int i = 0; i < arr.length; i++)
+		{
+			System.out.print("[" + arr[i] + "]");
+		}
+		System.out.println();
+	}
+}
