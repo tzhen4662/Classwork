9270fce3b78dd3bca78e7b69afab71ed86660760
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-12-20 13:16:39 -0500

    ZZZZZZ

--- ShapeLab/src/Lab4_2/CSVUtilities.java
+++ ShapeLab/src/Lab4_2/CSVUtilities.java
@@ -2,7 +2,8 @@ package Lab4_2;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException; 
+import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path; 
@@ -16,7 +17,16 @@ public class CSVUtilities {
 	
 	public CSVUtilities(File csv)
 	{
+		ArrayList<String> CSVData = new ArrayList<String>();
+		Path pathToFile = Paths.get("Water_Consumption_In_The_New_York_City.csv");
 		
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII))
+		{
+			String line = br.readLine();
+			while (line != null) 
+			{
+				String[] attributes = line.split(",");
+			}
 	}
 	
 	public List<String> getColumnHeaders()
